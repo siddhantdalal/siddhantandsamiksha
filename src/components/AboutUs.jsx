ec1feda6d8decc3e0f8fbe6c3ec82aa8b16b2cda
Siddhant S Dalal <siddhantsdalal@Siddhants-MacBook-Air.local>
2026-03-02 15:04:51 +0530

    Fix unused motion import lint warning

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+// eslint-disable-next-line no-unused-vars
 import { motion, AnimatePresence } from 'framer-motion';
 import { content } from '../data/content';
 import { useSide } from '../context/SideContext';
